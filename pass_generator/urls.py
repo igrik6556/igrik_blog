414bc72350c9f9934b61bddcd3307153ee747451
igrik6556 <igor.pivovarov6556@gmail.com>
2018-02-09 23:06:44 +0200

    Ajax in password generator app

--- pass_generator/urls.py
+++ pass_generator/urls.py
@@ -3,5 +3,6 @@ from pass_generator import views
 
 
 urlpatterns = [
-    url(r'^$', views.generator, name='generator'),
+    url(r'^$', views.generator_main, name='main'),
+    url(r'^generate/$', views.generate, name='generate'),
 ]
--- pass_generator/views.py
+++ pass_generator/views.py
@@ -1,17 +1,23 @@
+import json
 from django.shortcuts import render
+from django.http import Http404, HttpResponse
 from pass_generator.forms import GeneratorForm
 from pass_generator.models import Generator
 
 
-def generator(request):
+def generator_main(request):
+    return render(request,
+                  'pass_generator/main_gen.html',
+                  {"form": GeneratorForm()})
+
+
+def generate(request):
     if request.method == "POST":
         form = GeneratorForm(request.POST)
         if form.is_valid():
             c = form.save(commit=False)
-            return render(request, "pass_generator/main_gen.html", {"form": form,
-                                                                   "passwords": Generator.str_for_choice(c)})
+            response_data = dict(passwords=Generator.str_for_choice(c))
+            return HttpResponse(json.dumps(response_data),
+                                content_type='application/json')
     else:
-        form = GeneratorForm()
-    return render(request,
-                  'pass_generator/main_gen.html',
-                  {"form": form})
+        raise Http404
